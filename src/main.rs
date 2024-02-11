4ccbec0f91c09f7782466afffa98df55a023ac8c
Illian Yamandú <yamandu.developer@gmail.com>
2024-02-11 17:38:25 -0300

    First terms for kind

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![allow(unused)]
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct File {
+    name: String,
+    expression: Term,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Int {
+    value: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Str {
+    value: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Print {
+    value: Box<Term>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "Kind")]
+pub enum Term {
+    Int(Int),
+    Str(Str),
+    Print(Print),
+}
+
+#[derive(Debug)]
+pub enum Val {
+    Void,
+    Int(i32),
+    Bool(bool),
+    Str(String),
+}
+
+fn eval(term: Term) -> Val {
+    todo!()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
